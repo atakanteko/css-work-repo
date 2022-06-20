b7bab548781f000a4ab1ba49eec88fd1c8301984
atakantekoglu <atakantekoglu@gmail.com>
2022-06-21 00:44:32 +0300

    make css style file modular

--- src/pages/Timeline.js
+++ src/pages/Timeline.js
@@ -1,35 +1,35 @@
 import React from 'react';
-import '../styles/timeline.css';
+import style from '../styles/timeline.module.css';
 
 const Timeline = () => {
 
     return (
-        <div className='container'>
-            <div className='box'>
-                <ul className="timeline">
+        <div className={style.container}>
+            <div className={style.box}>
+                <ul>
                     <li>
-                        <div className="item">
+                        <div className={style.item}>
                             <span>3rd January 2022</span>
                             <h3>What is Lorem Ipsum?</h3>
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
                         </div>
                     </li>
                     <li>
-                        <div className="item">
+                        <div className={style.item}>
                             <span>3rd January 2022</span>
                             <h3>What is Lorem Ipsum?</h3>
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
                         </div>
                     </li>
                     <li>
-                        <div className="item">
+                        <div className={style.item}>
                             <span>3rd January 2022</span>
                             <h3>What is Lorem Ipsum?</h3>
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
                         </div>
                     </li>
                     <li>
-                        <div className="item">
+                        <div className={style.item}>
                             <span>3rd January 2022</span>
                             <h3>What is Lorem Ipsum?</h3>
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>

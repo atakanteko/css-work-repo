76564a445c60e331ac9646460500b5b431422c88
atakantekoglu <atakantekoglu@gmail.com>
2022-06-20 23:39:19 +0300

    init project and complete timeline

---
+++ src/pages/Timeline.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import '../styles/timeline.css';
+
+const Timeline = () => {
+
+    return (
+        <div className='container'>
+            <div className='box'>
+                <ul className="timeline">
+                    <li>
+                        <div className="item">
+                            <span>3rd January 2022</span>
+                            <h3>What is Lorem Ipsum?</h3>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="item">
+                            <span>3rd January 2022</span>
+                            <h3>What is Lorem Ipsum?</h3>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="item">
+                            <span>3rd January 2022</span>
+                            <h3>What is Lorem Ipsum?</h3>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                        </div>
+                    </li>
+                    <li>
+                        <div className="item">
+                            <span>3rd January 2022</span>
+                            <h3>What is Lorem Ipsum?</h3>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Timeline;

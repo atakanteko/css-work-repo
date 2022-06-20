920009d2a3061e3a69b052c322c837d2a6ce1abd
atakantekoglu <atakantekoglu@gmail.com>
2022-06-20 23:53:30 +0300

    initialize version 2 timeline

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes} from "react-router-dom";
 
 import Home from "./pages/Home";
 import Timeline from "./pages/Timeline";
+import TimelineVersion from "./pages/TimelineVersion";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/timeline" element={<Timeline />} />
+            <Route path="/timeline-version-2" element={<TimelineVersion />} />
         </Routes>
     </div>
   );
---
+++ src/pages/TimelineVersion.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../styles/timeline-v2.css';
+const TimelineVersion = () => {
+    return (
+        <div>
+            <h1>TimelineVersion</h1>
+        </div>
+    );
+};
+
+export default TimelineVersion;

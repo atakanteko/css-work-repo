e7692e470a8f494ee9c51d170df8e2fe9c95d30a
atakantekoglu <atakantekoglu@gmail.com>
2022-06-27 23:47:45 +0300

    image hover done

--- src/pages/ImageHover.js
+++ src/pages/ImageHover.js
@@ -14,6 +14,12 @@ const ImageHover = () => {
                                 <div className={style.imgBox}>
                                     <img src={item.photo} alt="Breaking Bad Photo"/>
                                 </div>
+                                <div className={style.details}>
+                                   <div className={style.content}>
+                                       <h2>What is Lorem Ipsum?</h2>
+                                       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+                                   </div>
+                                </div>
                             </div>
                         )
                     })
